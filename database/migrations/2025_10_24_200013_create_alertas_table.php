3469ec9c9a9e5396f793839491cb19e1d4b5b958
theoscar <oscardfer2020@gmail.com>
2025-10-24 15:09:31 -0500

    Actualización:aliminacion de la migracion de alertas y se creo una nueva migracio para una nueva tabla alertas mejor estructura

---
+++ database/migrations/2025_10_24_200013_create_alertas_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alertas', function (Blueprint $table) {
+            $table->id('id_alerta');
+
+            //  Llave foránea opcional para documento de vehículo
+            $table->unsignedBigInteger('id_documento_vehiculo')->nullable();
+            $table->foreign('id_documento_vehiculo')
+                ->references('id')
+                ->on('documentos_vehiculo')
+                ->onDelete('cascade');
+
+            //  Llave foránea opcional para documento de conductor
+            $table->unsignedBigInteger('id_documento_conductor')->nullable();
+            $table->foreign('id_documento_conductor')
+                ->references('id')
+                ->on('documentos_conductor')
+                ->onDelete('cascade');
+
+            //  Tipo de alerta y estado
+            $table->enum('tipo_alerta', ['Proximo a vencer', 'Vencido']);
+            $table->timestamp('fecha_generacion')->useCurrent();
+            $table->enum('estado', ['Pendiente', 'Atendida'])->default('Pendiente');
+
+            //  Timestamps de Laravel
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alertas');
+    }
+};
